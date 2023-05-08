505d0591c860a5f9bbea0bc563df6abd19661e7e
xyrlan; pedro <lobatopedro68@gmail.com>
2023-05-08 19:47:35 -0300

    transition components finished

--- src/pages/crew.tsx
+++ src/pages/crew.tsx
@@ -1,10 +1,16 @@
 import Commander from "@/components/Commander-crew";
 import Specialist from "@/components/Specialist-crew";
+import Pilot from "@/components/Pilot-crew";
+import Engineer from "@/components/Engineer-crew";
 
 
 
 import { Multistep } from "@/hooks/multistep"
 import { useState } from "react";
+import { CSSTransition } from 'react-transition-group'
+import { setTimeout } from "timers";
+
+
 
 
 export default function Crew() {
@@ -17,26 +23,62 @@ export default function Crew() {
       setSelectedButton(buttonValue);
     }
 
-    const MultiStepComponents = [<Commander />, <Specialist /> ]
+    const MultiStepComponents = [<Commander />, <Specialist />, <Pilot />, <Engineer /> ]
+
+    const [inProp, setInProp] = useState(false);
+
 
     return (
+        
         <>
         <section id="crew" className="font-genos md:bg-deskcrew max-sm:bg-mobilecrew sm:bg-tabletcrew h-screen flex flex-col justify-end bg-cover relative section">
-            <h1 className="text-4xl w-fit ml-[13%] absolute top-[20%]"><span className="text-[#a19797]">02</span> Meet your crew</h1>
-            <div>{MultiStepComponents[currentStep]}</div>
+            <h1 className="text-4xl w-fit ml-[13%] absolute top-[20%] "><span className="text-[#a19797]">02</span> Meet your crew</h1>
+            <CSSTransition
+                    in={inProp} // renderiza o componente atual
+                    timeout={500} // tempo em milissegundos da duração da animação
+                    classNames="fade" // classes CSS da animação
+            >{MultiStepComponents[currentStep]}</CSSTransition>
             <ul className="flex justify-between w-[8%] absolute bottom-[10%] left-[10%]">
+                <button   
+                onClick={() => {handleStepClick(0);
+                    setSelectedButton("Commander");
+                    setInProp(true);
+                    setTimeout(() => {
+                        setInProp(false);
+                    }, 500);
+ 
+                }}
+            className={` ${selectedButton === "Commander" ? 'bg-white h-3 w-3 scale-105 rounded-full shadow-lg' : 'h-3 w-3 scale-105 rounded-full bg-[#4e4747] shadow-lg'}`}></button>
                 <button 
-                onClick={() => {handleStepClick(0)}}
-                className="h-3 w-3 scale-105 rounded-full bg-[#a19797] shadow-lg"></button>
+                onClick={() => {handleStepClick(1);
+                    setSelectedButton("Specialist");
+                    setInProp(true);
+                    setTimeout(() => {
+                        setInProp(false);
+                    }, 500);
+                    
+                }}
+                className={`${selectedButton === "Specialist" ? 'bg-white h-3 w-3 scale-105 rounded-full shadow-lg' : 'h-3 w-3 scale-105 rounded-full bg-[#4e4747] shadow-lg'}`}></button>
                 <button 
-                onClick={() => {handleStepClick(1)}}
-                className="h-3 w-3 scale-105 rounded-full bg-[#a19797] shadow-lg"></button>
+                onClick={() => {handleStepClick(2);
+                    setSelectedButton("Pilot");
+                    setInProp(true);
+                    setTimeout(() => {
+                        setInProp(false);
+                    }, 500);
+                    
+                }}
+                className={`${selectedButton === "Pilot" ? 'bg-white h-3 w-3 scale-105 rounded-full shadow-lg' : 'h-3 w-3 scale-105 rounded-full bg-[#4e4747] shadow-lg'}`}></button>
                 <button 
-                onClick={() => {handleStepClick(2)}}
-                className="h-3 w-3 scale-105 rounded-full bg-[#a19797] shadow-lg"></button>
-                <button 
-                onClick={() => {handleStepClick(3)}}
-                className="h-3 w-3 scale-105 rounded-full bg-[#a19797] shadow-lg"></button>
+                onClick={() => {handleStepClick(3);
+                    setSelectedButton("Engineer");
+                    setInProp(true);
+                    setTimeout(() => {
+                        setInProp(false);
+                    }, 500);
+                    
+                }}
+                className={` ${selectedButton === "Engineer" ? 'bg-white h-3 w-3 scale-105 rounded-full shadow-lg' : 'h-3 w-3 scale-105 rounded-full bg-[#4e4747] shadow-lg'}`}></button>
             </ul>
 
         </section>
