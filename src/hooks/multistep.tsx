526f3dc7e6a01a3bc1caa11d1915851143a49830
xyrlan; pedro <lobatopedro68@gmail.com>
2023-05-08 09:26:01 -0300

    imports multstep update

---
+++ src/hooks/multistep.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { useState } from "react";
+
+
+    export function Multistep(stepLenght: number) {
+
+
+    const [currentStep, setCurrentStep] = useState(0);
+    // if (currentStep === stepLenght - 1) return;
+    // if (currentStep === 0) return;
+
+    console.log(currentStep);
+
+    const handleStepClick = (step: number) => {
+        setCurrentStep(step);
+    };
+
+    return {
+        currentStep,
+        handleStepClick,
+        
+
+    };
+};
