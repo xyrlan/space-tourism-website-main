446c23f4bef4620e5b8248e4582c334ef4f163d1
xyrlan; pedro <lobatopedro68@gmail.com>
2023-05-08 20:25:30 -0300

    update bg images

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,25 @@ module.exports = {
           'deskhome': "url('/images/background-home-desktop.jpg')",
           'tablethome': "url('/images/background-home-tablet.jpg')",
           'mobilehome': "url('/images/background-home-mobile.jpg')",
+          'deskdestination': "url('/images/background-destination-desktop.jpg')",
+          'tabletdestination': "url('/images/background-destination-tablet.jpg')",
+          'mobiledestination': "url('/images/background-destination-mobile.jpg')",
+          'deskcrew': "url('/images/background-crew-desktop.jpg')",
+          'tabletcrew': "url('/images/background-crew-tablet.jpg')",
+          'mobilecrew': "url('/images/background-crew-mobile.jpg')",
+          'desktech': "url('/images/background-technology-desktop.jpg')",
+          'tablettech': "url('/images/background-technology-tablet.jpg')",
+          'mobiletech': "url('/images/background-technology-mobile.jpg')",
       },
+      animation: {
+        wiggle: 'wiggle 1s ease-in-out',
+        
+      },
+    },
+    fontFamily: {
+      brunoace: ["Bruno Ace SC", "cursive"],
+      genos: ["Genos", "sans-serif"],
+      
     },
   },
   plugins: [],
