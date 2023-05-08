97170da951e750c6ac4463e8d2fd7207110533c6
xyrlan; pedro <lobatopedro68@gmail.com>
2023-05-08 11:15:14 -0300

    update h1 to absolute

--- src/pages/destination.tsx
+++ src/pages/destination.tsx
@@ -19,7 +19,7 @@ export default function Destination() {
 
     
 
-    const [selectedButton, setSelectedButton] = useState("0");
+    const [selectedButton, setSelectedButton] = useState("Moon");
 
     function handleButtonClick(buttonValue: any) {
       setSelectedButton(buttonValue);
@@ -35,28 +35,28 @@ export default function Destination() {
         <>
         <section id="destination" className="font-genos md:bg-deskdestination max-sm:bg-mobiledestination sm:bg-tabletdestination h-screen flex flex-col justify-end pb-10 bg-cover relative section">
             
-                <h1 className="text-4xl w-fit ml-[13%]"><span className="text-[#a19797]">01</span> Pick your destination</h1>
+                <h1 className="text-4xl w-fit ml-[13%] mb-[5%] absolute top-[20%]"><span className="text-[#a19797]">01</span> Pick your destination</h1>
 
                 <div className="text-xl flex justify-end xl:w-[76%] lg:w-[80%] md:w-[90%] space-x-10 select-none">
 
                 <div onClick={() => { 
                 handleStepClick(0); 
-                handleButtonClick("0");}} className={` hover:border-[#a19797] hover:border-b-2 cursor-pointer h-10 duration-500 ${selectedButton === "0" ? 'hover:border-white border-white border-solid border-b-2 ' : 'border-white border-solid border-b-2 border-opacity-0'} `}>
+                handleButtonClick("0");}} className={` hover:border-[#a19797] hover:border-b-2 cursor-pointer h-10 duration-500 ${selectedButton === "Moon" ? 'hover:border-white border-white border-solid border-b-2 ' : 'border-white border-solid border-b-2 border-opacity-0'} `}>
                 <button>Moon</button>
                 </div>
                 <div onClick={() => { 
                 handleStepClick(1); 
-                handleButtonClick("1");}} className={`hover:border-[#a19797] hover:border-b-2 cursor-pointer h-10 duration-500 ${selectedButton === "1" ? 'hover:border-white border-white border-solid border-b-2 ' : 'border-white border-solid border-b-2 border-opacity-0'} `}>
+                handleButtonClick("1");}} className={`hover:border-[#a19797] hover:border-b-2 cursor-pointer h-10 duration-500 ${selectedButton === "Mars" ? 'hover:border-white border-white border-solid border-b-2 ' : 'border-white border-solid border-b-2 border-opacity-0'} `}>
                 <button>Mars</button>
                 </div>
                 <div onClick={() => { 
                 handleStepClick(2); 
-                handleButtonClick("2");}} className={`hover:border-[#a19797] hover:border-b-2 cursor-pointer h-10 duration-500 ${selectedButton === "2" ? 'hover:border-white border-white border-solid border-b-2 ' : 'border-white border-solid border-b-2 border-opacity-0'} `}>
+                handleButtonClick("2");}} className={`hover:border-[#a19797] hover:border-b-2 cursor-pointer h-10 duration-500 ${selectedButton === "Europa" ? 'hover:border-white border-white border-solid border-b-2 ' : 'border-white border-solid border-b-2 border-opacity-0'} `}>
                 <button>Europa</button>
                 </div>
             <div onClick={() => { 
                 handleStepClick(3); 
-                handleButtonClick("3");}} className={`hover:border-[#a19797] hover:border-b-2 cursor-pointer h-10 duration-500 ${selectedButton === "3" ? 'hover:border-white border-white border-solid border-b-2 ' : 'border-white border-solid border-b-2 border-opacity-0'} `}>
+                handleButtonClick("3");}} className={`hover:border-[#a19797] hover:border-b-2 cursor-pointer h-10 duration-500 ${selectedButton === "Titan" ? 'hover:border-white border-white border-solid border-b-2 ' : 'border-white border-solid border-b-2 border-opacity-0'} `}>
             <button >Titan</button>
 
             </div>
