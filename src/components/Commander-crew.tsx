8142a2a279fa9a09a5cbd8ca1c693ed9462434ab
xyrlan; pedro <lobatopedro68@gmail.com>
2023-05-08 11:01:21 -0300

    step commander crew

---
+++ src/components/Commander-crew.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image"
+
+export default function Commander() {
+    return (
+        <div className="flex items-center px-[10%] gap-[10%]">
+            
+            <div className="w-[40%]">            
+                <h1 className="text-[#a19797] text-4xl">Commander</h1>
+                <h2 className="text-6xl">Douglas Hurley</h2>
+
+                <p className="text-lg my-[5%]">
+                    Douglas Gerald Hurley is an American engineer, former Marine Corps pilot 
+                    and former NASA astronaut. He launched into space for the third time as 
+                    commander of Crew Dragon Demo-2.
+                </p>
+            </div>
+
+            
+            <Image className="select-none hover:opacity-95 hover:brightness-100 brightness-90 duration-500" src="/images/image-douglas-hurley.png" width={400} height={400} alt="douglas hurley" />
+        </div>
+    )
+}
