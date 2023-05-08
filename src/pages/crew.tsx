bd2ec5ce53bb1d1e885bc273c2eab556b933c01e
xyrlan; pedro <lobatopedro68@gmail.com>
2023-05-08 11:15:36 -0300

    update h1 to absolute

--- src/pages/crew.tsx
+++ src/pages/crew.tsx
@@ -1,4 +1,8 @@
 import Commander from "@/components/Commander-crew";
+import Specialist from "@/components/Specialist-crew";
+
+
+
 import { Multistep } from "@/hooks/multistep"
 import { useState } from "react";
 
@@ -13,18 +17,26 @@ export default function Crew() {
       setSelectedButton(buttonValue);
     }
 
-    const MultiStepComponents = [<Commander /> ]
+    const MultiStepComponents = [<Commander />, <Specialist /> ]
 
     return (
         <>
         <section id="crew" className="font-genos md:bg-deskcrew max-sm:bg-mobilecrew sm:bg-tabletcrew h-screen flex flex-col justify-end bg-cover relative section">
-            <h1 className="text-4xl w-fit ml-[13%] "><span className="text-[#a19797]">02</span> Meet your crew</h1>
+            <h1 className="text-4xl w-fit ml-[13%] absolute top-[20%]"><span className="text-[#a19797]">02</span> Meet your crew</h1>
             <div>{MultiStepComponents[currentStep]}</div>
             <ul className="flex justify-between w-[8%] absolute bottom-[10%] left-[10%]">
-                <button className="h-3 w-3 scale-105 rounded-full bg-[#a19797] shadow-lg"></button>
-                <button className="h-3 w-3 scale-105 rounded-full bg-[#a19797] shadow-lg"></button>
-                <button className="h-3 w-3 scale-105 rounded-full bg-[#a19797] shadow-lg"></button>
-                <button className="h-3 w-3 scale-105 rounded-full bg-[#a19797] shadow-lg"></button>
+                <button 
+                onClick={() => {handleStepClick(0)}}
+                className="h-3 w-3 scale-105 rounded-full bg-[#a19797] shadow-lg"></button>
+                <button 
+                onClick={() => {handleStepClick(1)}}
+                className="h-3 w-3 scale-105 rounded-full bg-[#a19797] shadow-lg"></button>
+                <button 
+                onClick={() => {handleStepClick(2)}}
+                className="h-3 w-3 scale-105 rounded-full bg-[#a19797] shadow-lg"></button>
+                <button 
+                onClick={() => {handleStepClick(3)}}
+                className="h-3 w-3 scale-105 rounded-full bg-[#a19797] shadow-lg"></button>
             </ul>
 
         </section>
